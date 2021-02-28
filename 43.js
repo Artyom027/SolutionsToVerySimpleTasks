8f369d57fceb6fa886723a9ebef8230783dd226b
Artyom027 <77734679+Artyom027@users.noreply.github.com>
2021-02-28 18:34:42 +0400

    Create 43.js

---
+++ 43.js
@@ -0,0 +1,8 @@
+function getFilledArray(text,numberՕfRepetitions) 
+{
+    let arr = [];
+    for(let i = 0; i < numberՕfRepetitions; i++)
+    arr[i] = text;
+    console.log(arr);
+}
+getFilledArray('barv',5)
